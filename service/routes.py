5ec97c972c9fd49849dceb763e45561e587b23c4
sgp9468 <124538276+sgp9468@users.noreply.github.com>
2023-03-16 13:38:20 -0400

    Update routes.py
    Updating route.py with delete customer feature

--- service/routes.py
+++ service/routes.py
@@ -61,3 +61,21 @@ def create_customers():
 
     app.logger.info("Customer with ID [%s] created.", customer.id)
     return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+
+######################################################################
+# DELETE A CUSTOMER
+######################################################################
+@app.route("/customers/<int:customer_id>", methods=["DELETE"])
+def delete_customers(customer_id):
+    """
+    Delete a Customer
+    This endpoint will delete a Customer based on the id specified in the path
+    """
+    app.logger.info("Request to delete Customer with id: %s", customer_id)
+    customer = customer.find(customer_id)
+    if customer:
+        customer.delete()
+
+    app.logger.info("Customer with ID [%s] delete complete.", customer_id)
+    return "", status.HTTP_204_NO_CONTENT
+
