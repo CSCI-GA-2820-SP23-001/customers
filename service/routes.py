c9ebbe421973dda6e8f8a1658a8569398be11f37
mahak3191 <mahak.chhajer@gmail.com>
2023-03-20 17:29:53 +0000

    initial version of list-customers

--- service/routes.py
+++ service/routes.py
@@ -17,6 +17,14 @@ from service.models import Customer
 # Import Flask application
 from . import app
 
+######################################################################
+# GET HEALTH CHECK
+######################################################################
+@app.route("/healthcheck")
+def healthcheck():
+    """Let them know our heart is still beating"""
+    return jsonify(status=200, message="Healthy"), status.HTTP_200_OK
+
 
 ######################################################################
 # GET INDEX
@@ -41,6 +49,22 @@ def index():
 
 # Place your REST API code here ...
 
+######################################################################
+# RETRIEVE A CUSTOMER
+######################################################################
+@app.route("/customers/<int:id>", methods=["GET"])
+def get_customers(id):
+    """
+    Retrieve a single customer
+    This endpoint will return a Customer based on it's id
+    """
+    app.logger.info("Request for customer with id: %s", id)
+    customer = Customer.find(id)
+    if not customer:
+        abort(status.HTTP_404_NOT_FOUND, f"Customer with id '{id}' was not found.")
+
+    app.logger.info("Returning customer: %s", customer.first_name)
+    return jsonify(customer.serialize()), status.HTTP_200_OK
 
 ######################################################################
 # ADD A NEW CUSTOMER
@@ -57,7 +81,46 @@ def create_customers():
     customer.deserialize(request.get_json())
     customer.create()
     message = customer.serialize()
-    location_url = url_for("get_customers", customer_id=customer.id, _external=True)
+    location_url = url_for("get_customers", id=customer.id, _external=True)
 
     app.logger.info("Customer with ID [%s] created.", customer.id)
     return jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
+
+# LIST A CUSTOMER
+
+@app.route("/customers", methods=["GET"])
+def list_customers():
+    """Returns all of the Customers"""
+    app.logger.info("Request for customer list")
+    customers = []
+    email = request.args.get("email")
+    if email:
+        customers = Customer.find_by_email(email)
+    else:
+        customers = Customer.all()
+    results = [customer.serialize() for customer in customers]
+    app.logger.info("Returning %d customers", len(results))
+    return jsonify(results), status.HTTP_200_OK 
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+
+def check_content_type(content_type):
+    """Checks that the media type is correct"""
+    if "Content-Type" not in request.headers:
+        app.logger.error("No Content-Type specified.")
+        abort(
+            status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            f"Content-Type must be {content_type}",
+        )
+
+    if request.headers["Content-Type"] == content_type:
+        return
+
+    app.logger.error("Invalid Content-Type: %s", request.headers["Content-Type"])
+    abort(
+        status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+        f"Content-Type must be {content_type}",
+    )
