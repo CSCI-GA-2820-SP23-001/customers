3e6863462579942bedeb1f4971813b2fba2278e7
sgp9468 <124538276+sgp9468@users.noreply.github.com>
2023-04-03 21:00:05 -0400

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -367,3 +367,19 @@ class TestCustomerServer(TestCase):
         # activate the customer
         response = self.client.put(f"{BASE_URL}/0/activate")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        
+    def test_query_customer_list_by_name(self):
+        """It should query customers by name"""
+        customers = self._create_customers(10)
+        test_name = customers[0].name
+        name_customers = [customer for customer in customers if customer.name == test_name]
+        response = self.client.get(
+            BASE_URL,
+            query_string=f"name={quote_plus(test_name)}"
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), len(name_customers))
+        # check the data just to be sure
+        for customer in data:
+            self.assertEqual(customer["name"], test_name)
