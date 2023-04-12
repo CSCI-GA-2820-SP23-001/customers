1c8771349bdda22fa196e0a20f5a13ed580db84e
Noah Wolfenzon <nwolfenzon@gmail.com>
2023-04-12 15:44:52 -0400

    updating index.html ui for CRUD operations. adding bdd scaffolding

---
+++ service/static/js/rest_api.js
@@ -0,0 +1,256 @@
+$(function () {
+
+
+
+    // *********************
+    //  C O N S T A N T S
+    // *********************
+    const STATUS = {
+        ACTIVE: 'ACTIVE',
+        SUSPENDED: 'SUSPENDED'
+    };
+
+    const API_URL = 'http://http://159.122.187.192:31000/customers';
+
+    // ****************************************
+    //  U T I L I T Y   F U N C T I O N S
+    // ****************************************
+
+    // Updates the form with data from the response
+    function update_form_data(res) {
+        $("#customer_id").val(res.id);
+        $("#customer_first_name").val(res.first_name);
+        $("#customer_last_name").val(res.last_name);
+        $("#customer_email").val(res.email);
+        $("#customer_password").val(res.password);
+        $("#customer_status").val(res.status)
+    }
+
+    /// Clears all form fields
+    function clear_form_data() {
+        $("#customer_first_name").val("");
+        $("#customer_last_name").val("");
+        $("#customer_email").val("");
+        $("#customer_password").val("");
+        $("#customer_status").val(STATUS.ACTIVE);
+    }
+
+    // Updates the flash message area
+    function flash_message(message) {
+        $("#flash_message").empty();
+        $("#flash_message").append(message);
+    }
+
+    // ****************************************
+    // Create a customer
+    // ****************************************
+
+    $("#create-btn").click(function () {
+
+        let first_name = $("#customer_first_name").val();
+        let last_name = $("#customer_last_name").val();
+        let email = $("#customer_email").val();
+        let password = $("#customer_password").val();
+        let status = $("#customer_status").val();
+
+        let data = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "email": email,
+            "password": password,
+            "status": status
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "POST",
+            url: "/customers",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+        });
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+    });
+
+
+    // ****************************************
+    // Update a customer
+    // ****************************************
+
+    $("#update-btn").click(function () {
+
+        let customer_id = $("#customer_id").val();
+        let name = $("#customer_name").val();
+        let category = $("#customer_category").val();
+        let available = $("#customer_available").val() == "true";
+        let gender = $("#customer_gender").val();
+        let birthday = $("#customer_birthday").val();
+
+        let data = {
+            "name": name,
+            "category": category,
+            "available": available,
+            "gender": gender,
+            "birthday": birthday
+        };
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+                type: "PUT",
+                url: `/customers/${customer_id}`,
+                contentType: "application/json",
+                data: JSON.stringify(data)
+            })
+
+        ajax.done(function(res){
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Retrieve a customer
+    // ****************************************
+
+    $("#retrieve-btn").click(function () {
+
+        let customer_id = $("#customer_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/customers/${customer_id}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            update_form_data(res)
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            clear_form_data()
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+    // ****************************************
+    // Delete a customer
+    // ****************************************
+
+    $("#delete-btn").click(function () {
+
+        let customer_id = $("#customer_id").val();
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "DELETE",
+            url: `/customers/${customer_id}`,
+            contentType: "application/json",
+            data: '',
+        })
+
+        ajax.done(function(res){
+            clear_form_data()
+            flash_message("customer has been Deleted!")
+        });
+
+        ajax.fail(function(res){
+            flash_message("Server error!")
+        });
+    });
+
+    // ****************************************
+    // Clear the form
+    // ****************************************
+
+    $("#clear-btn").click(function () {
+        $("#customer_id").val("");
+        $("#flash_message").empty();
+        clear_form_data()
+    });
+
+    // ****************************************
+    // Search for a customer
+    // ****************************************
+
+    $("#search-btn").click(function () {
+
+        let first_name = $("#customer_first_name").val();
+        let email = $("#customer_email").val();
+
+        let queryStrings = []
+
+        if (first_name) {
+            queryStrings.push('first_name=' + first_name)
+        }
+        if (email) {
+            queryStrings.push('email=' + email)
+        }
+
+        $("#flash_message").empty();
+
+        let ajax = $.ajax({
+            type: "GET",
+            url: `/customers?${queryStrings.join('&')}`,
+            contentType: "application/json",
+            data: ''
+        })
+
+        ajax.done(function(res){
+            //alert(res.toSource())
+            $("#search_results").empty();
+            let table = '<table class="table table-striped" cellpadding="10">'
+            table += '<thead><tr>'
+            table += '<th class="col-md-1">ID</th>'
+            table += '<th class="col-md-3">First Name</th>'
+            table += '<th class="col-md-4">Last Name</th>'
+            table += '<th class="col-md-4">Email</th>'
+            table += '<th class="col-md-3">Password</th>'
+            table += '<th class="col-md-3">Status</th>'
+            table += '</tr></thead><tbody>'
+            let firstCustomer = "";
+            for(let i = 0; i < res.length; i++) {
+                let customer = res[i];
+                table +=  `<tr id="row_${i}"><td>${customer.id}</td><td>${customer.first_name}</td><td>${customer.last_name}</td><td>${customer.email}</td><td>${customer.password}</td><td>${customer.status}</td></tr>`;
+                if (i == 0) {
+                    firstCustomer = customer;
+                }
+            }
+            table += '</tbody></table>';
+            $("#search_results").append(table);
+
+            // copy the first result to the form
+            if (firstCustomer != "") {
+                update_form_data(firstCustomer)
+            }
+
+            flash_message("Success")
+        });
+
+        ajax.fail(function(res){
+            flash_message(res.responseJSON.message)
+        });
+
+    });
+
+})
