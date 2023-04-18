01bc5e75bc8404c4e913c9508f067966cc726a43
mahak3191 <mahak.chhajer@gmail.com>
2023-04-18 00:13:34 +0000

    Bdd test for create customer

--- features/customer.feature
+++ features/customer.feature
@@ -17,28 +17,28 @@ Scenario: The server is running
     Then I should see "Customers Admin Portal" in the title
     And I should not see "404 Not Found"
 
-# Scenario: Create a Pet
-#     When I visit the "Home Page"
-#     And I set the "Name" to "Happy"
-#     And I set the "Category" to "Hippo"
-#     And I select "False" in the "Available" dropdown
-#     And I select "Male" in the "Gender" dropdown
-#     And I set the "Birthday" to "06-16-2022"
-#     And I press the "Create" button
-#     Then I should see the message "Success"
-#     When I copy the "Id" field
-#     And I press the "Clear" button
-#     Then the "Id" field should be empty
-#     And the "Name" field should be empty
-#     And the "Category" field should be empty
-#     When I paste the "Id" field
-#     And I press the "Retrieve" button
-#     Then I should see the message "Success"
-#     And I should see "Happy" in the "Name" field
-#     And I should see "Hippo" in the "Category" field
-#     And I should see "False" in the "Available" dropdown
-#     And I should see "Male" in the "Gender" dropdown
-#     And I should see "2022-06-16" in the "Birthday" field
+Scenario: Create a Customer
+     When I visit the "home page"
+     And I set the "First Name" to "abraham"
+     And I set the "Last Name" to "abrahamson"
+     And I set the "Email" to "aabrahamson@dishonest.com"
+     And I set the "Password" to "th3honest1" 
+     And I select "Active" in the "Status" dropdown
+     And I press the "Create" button
+     And I wait for 2 seconds
+     Then I should see the message "Success"
+     When I copy the "First Name" field
+     And I press the "Clear" button
+     Then the "First Name" field should be empty
+     And the "Last name" field should be empty
+     And the "Email" field should be empty
+     When I paste the "First Name" field
+     And I press the "Retrieve" button
+     Then I should see the message "Success"
+     And I should see "abrahamson" in the "Last Name" field
+     And I should see "aabrahamson@honest.com" in the "Email" field
+     And I should see "th3honest1" in the "Password" field
+     And I should see "Active" in the "Status" dropdown
 
 # Scenario: List all pets
 #     When I visit the "Home Page"
--- service/static/js/rest_api.js
+++ service/static/js/rest_api.js
@@ -1,7 +1,6 @@
 $(function () {
 
-
-
+    
     // *********************
     //  C O N S T A N T S
     // *********************
@@ -9,19 +8,17 @@ $(function () {
         ACTIVE: 'ACTIVE',
         SUSPENDED: 'SUSPENDED'
     };
-
     // ****************************************
     //  U T I L I T Y   F U N C T I O N S
     // ****************************************
 
     // Updates the form with data from the response
     function update_form_data(res) {
-        $("#customer_id").val(res.id);
         $("#customer_first_name").val(res.first_name);
         $("#customer_last_name").val(res.last_name);
         $("#customer_email").val(res.email);
         $("#customer_password").val(res.password);
-        $("#customer_status").val(res.status)
+        $("#customer_status").val(res.status);
     }
 
     /// Clears all form fields
@@ -30,7 +27,7 @@ $(function () {
         $("#customer_last_name").val("");
         $("#customer_email").val("");
         $("#customer_password").val("");
-        $("#customer_status").val(STATUS.ACTIVE);
+        $("#customer_status").val("STATUS.ACTIVE");
     }
 
     // Updates the flash message area
@@ -40,7 +37,7 @@ $(function () {
     }
 
     // ****************************************
-    // Create a customer
+    // Create a Customer
     // ****************************************
 
     $("#create-btn").click(function () {
@@ -60,7 +57,7 @@ $(function () {
         };
 
         $("#flash_message").empty();
-
+        
         let ajax = $.ajax({
             type: "POST",
             url: "/customers",
@@ -77,178 +74,3 @@ $(function () {
             flash_message(res.responseJSON.message)
         });
     });
-
-
-    // ****************************************
-    // Update a customer
-    // ****************************************
-
-    $("#update-btn").click(function () {
-
-        let customer_id = $("#customer_id").val();
-        let name = $("#customer_name").val();
-        let category = $("#customer_category").val();
-        let available = $("#customer_available").val() == "true";
-        let gender = $("#customer_gender").val();
-        let birthday = $("#customer_birthday").val();
-
-        let data = {
-            "name": name,
-            "category": category,
-            "available": available,
-            "gender": gender,
-            "birthday": birthday
-        };
-
-        $("#flash_message").empty();
-
-        let ajax = $.ajax({
-                type: "PUT",
-                url: `/customers/${customer_id}`,
-                contentType: "application/json",
-                data: JSON.stringify(data)
-            })
-
-        ajax.done(function(res){
-            update_form_data(res)
-            flash_message("Success")
-        });
-
-        ajax.fail(function(res){
-            flash_message(res.responseJSON.message)
-        });
-
-    });
-
-    // ****************************************
-    // Retrieve a customer
-    // ****************************************
-
-    $("#retrieve-btn").click(function () {
-
-        let customer_id = $("#customer_id").val();
-
-        $("#flash_message").empty();
-
-        let ajax = $.ajax({
-            type: "GET",
-            url: `/customers/${customer_id}`,
-            contentType: "application/json",
-            data: ''
-        })
-
-        ajax.done(function(res){
-            //alert(res.toSource())
-            update_form_data(res)
-            flash_message("Success")
-        });
-
-        ajax.fail(function(res){
-            clear_form_data()
-            flash_message(res.responseJSON.message)
-        });
-
-    });
-
-    // ****************************************
-    // Delete a customer
-    // ****************************************
-
-    $("#delete-btn").click(function () {
-
-        let customer_id = $("#customer_id").val();
-
-        $("#flash_message").empty();
-
-        let ajax = $.ajax({
-            type: "DELETE",
-            url: `/customers/${customer_id}`,
-            contentType: "application/json",
-            data: '',
-        })
-
-        ajax.done(function(res){
-            clear_form_data()
-            flash_message("customer has been Deleted!")
-        });
-
-        ajax.fail(function(res){
-            flash_message("Server error!")
-        });
-    });
-
-    // ****************************************
-    // Clear the form
-    // ****************************************
-
-    $("#clear-btn").click(function () {
-        $("#customer_id").val("");
-        $("#flash_message").empty();
-        clear_form_data()
-    });
-
-    // ****************************************
-    // Search for a customer
-    // ****************************************
-
-    $("#search-btn").click(function () {
-
-        let first_name = $("#customer_first_name").val();
-        let email = $("#customer_email").val();
-
-        let queryStrings = []
-
-        if (first_name) {
-            queryStrings.push('first_name=' + first_name)
-        }
-        if (email) {
-            queryStrings.push('email=' + email)
-        }
-
-        $("#flash_message").empty();
-
-        let ajax = $.ajax({
-            type: "GET",
-            url: `/customers?${queryStrings.join('&')}`,
-            contentType: "application/json",
-            data: ''
-        })
-
-        ajax.done(function(res){
-            //alert(res.toSource())
-            $("#search_results").empty();
-            let table = '<table class="table table-striped" cellpadding="10">'
-            table += '<thead><tr>'
-            table += '<th class="col-md-1">ID</th>'
-            table += '<th class="col-md-3">First Name</th>'
-            table += '<th class="col-md-4">Last Name</th>'
-            table += '<th class="col-md-4">Email</th>'
-            table += '<th class="col-md-3">Password</th>'
-            table += '<th class="col-md-3">Status</th>'
-            table += '</tr></thead><tbody>'
-            let firstCustomer = "";
-            for(let i = 0; i < res.length; i++) {
-                let customer = res[i];
-                table +=  `<tr id="row_${i}"><td>${customer.id}</td><td>${customer.first_name}</td><td>${customer.last_name}</td><td>${customer.email}</td><td>${customer.password}</td><td>${customer.status}</td></tr>`;
-                if (i == 0) {
-                    firstCustomer = customer;
-                }
-            }
-            table += '</tbody></table>';
-            $("#search_results").append(table);
-
-            // copy the first result to the form
-            if (firstCustomer != "") {
-                update_form_data(firstCustomer)
-            }
-
-            flash_message("Success")
-        });
-
-        ajax.fail(function(res){
-            flash_message(res.responseJSON.message)
-        });
-
-    });
-
-})
