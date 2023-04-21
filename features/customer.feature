ca2e591e58c39b3d9aae6462fb8d1871a30f1753
Noah Wolfenzon <nwolfenzon@gmail.com>
2023-04-21 11:14:19 -0400

    adding reqd test

--- features/customer.feature
+++ features/customer.feature
@@ -118,8 +118,14 @@ Scenario: Deleting a customer
 
 Scenario: Retrieve a Customer
     When I visit the "home page"
-    And I set the "id" to "1"
+    And I press the "Search" button
+    And I copy the customer in the form
+    And I press the "Clear" button
+    Then the "id" field should be empty
+    And the "first_name" field should be empty
+    When I paste the customer "id" field
     And I press the "Retrieve" button
     Then I should see the message "Success"
-    And I should see "abraham" in the "first_name" field
-    And I should see "abrahamson" in the "last_name" field
+    And the "first_name" should equal the copied customer
+    And the "last_name" should equal the copied customer
+    And the "email" should equal the copied customer
--- features/steps/customers_steps.py
+++ features/steps/customers_steps.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 ######################################################################
 
+# pylint: disable=function-redefined, missing-function-docstring
 """
 Customer Steps
 
@@ -22,10 +23,15 @@ Steps file for Customer.feature
 For information on Waiting until elements are present in the HTML see:
     https://selenium-python.readthedocs.io/waits.html
 """
+import logging
 import requests
-from behave import given
+from behave import given, then, when
 from compare import expect
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 
+ID_PREFIX = 'customer_'
 
 @given('the following customers')
 def step_impl(context):
@@ -49,3 +55,55 @@ def step_impl(context):
         }
         context.resp = requests.post(rest_endpoint, json=payload)
         expect(context.resp.status_code).to_equal(201)
+
+@when('I copy the customer in the form')
+def step_impl(context):
+    """Copy the entire customer object in the form component"""
+
+    element_keys = [
+        'id',
+        'first_name',
+        'last_name',
+        'email',
+        'password',
+        'status'
+    ]
+    element_ids = [ID_PREFIX + el for el in element_keys]
+
+    customer = {}
+    for el_id, el_key in zip(element_ids, element_keys):
+        element = context.driver.find_element(By.ID, el_id)
+        customer[el_key] = element.get_attribute('value')
+
+    context.clipboard = customer
+    logging.info('Clipboard contains: %s', context.clipboard)
+
+@when('I paste the customer "{element_name}" field')
+def step_impl(context, element_name):
+    """Pastes the field element_name field into the corresponding form component"""
+
+    element_key = element_name.lower().replace(' ', '_')
+    element_id = ID_PREFIX + element_key
+    element = WebDriverWait(context.driver, context.WAIT_SECONDS).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+
+    value = context.clipboard.get(element_key, '')
+
+    element.clear()
+    element.send_keys(value)
+
+@then('the "{element_name}" should equal the copied customer')
+def step_impl(context, element_name):
+    """Compares the form field element_name to the copied customer element_name field"""
+
+    element_key = element_name.lower().replace(' ', '_')
+    element_id = ID_PREFIX + element_key
+    element = WebDriverWait(context.driver, context.WAIT_SECONDS).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+
+    form_value = element.get_attribute('value')
+    copied_value = context.clipboard.get(element_key, '')
+
+    expect(form_value).to_equal(copied_value)
